d287a06a7cfcbd8306d9e00e941aac5607019bc6
congyh <yihao.cong@outlook.com>
2017-04-19 20:50:03 +0800

    [WIP] turbine监控功能

--- turbine/src/main/java/com/github/congyh/TurbineApplication.java
+++ turbine/src/main/java/com/github/congyh/TurbineApplication.java
@@ -2,7 +2,13 @@ package com.github.congyh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+@EnableDiscoveryClient
+@EnableTurbine
+@EnableHystrixDashboard
 @SpringBootApplication
 public class TurbineApplication {
 
