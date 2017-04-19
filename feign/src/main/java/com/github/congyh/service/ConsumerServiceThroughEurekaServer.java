b37a762b517fd05db17a8ca104815d006f60f042
congyh <yihao.cong@outlook.com>
2017-04-19 20:50:29 +0800

    [WIP] feign与Hystrix结合

--- feign/src/main/java/com/github/congyh/service/ConsumerServiceThroughEurekaServer.java
+++ feign/src/main/java/com/github/congyh/service/ConsumerServiceThroughEurekaServer.java
@@ -1,5 +1,6 @@
 package com.github.congyh.service;
 
+import com.github.congyh.service.impl.ConsumerServiceThroughEurekaServerImpl;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * @author <a href='mailto:yihao.cong@outlook.com'>Cong Yihao</a>
  */
-//@FeignClient(value = "EUREKA-CLIENT-SERVICE", fallback = ConsumerServiceThroughEurekaServerImpl.class) // 自带负载均衡, 底层也是通过ribbon实现的, 这里还添加了Hystrix熔断支持
-@FeignClient(value = "EUREKA-CLIENT-SERVICE")
+@FeignClient(value = "EUREKA-CLIENT-SERVICE", fallback = ConsumerServiceThroughEurekaServerImpl.class) // 自带负载均衡, 底层也是通过ribbon实现的, 这里还添加了Hystrix熔断支持
+//@FeignClient(value = "EUREKA-CLIENT-SERVICE")
 public interface ConsumerServiceThroughEurekaServer {
 
     // 这里注意接口定义要与被消费的REST服务一致
--- feign/src/main/java/com/github/congyh/web/ConsumerController.java
+++ feign/src/main/java/com/github/congyh/web/ConsumerController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConsumerController {
 
     @Autowired
+//    @Qualifier("consumerServiceThroughEurekaServer")
     private ConsumerServiceThroughEurekaServer serviceProvider;
 
     @GetMapping(value = "/add")
