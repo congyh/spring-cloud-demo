3e423152edd9641191cdf59665864dda2001a3ae
congyh <yihao.cong@outlook.com>
2017-04-20 09:19:32 +0800

    [WIP] OAuth Authorization Server

--- oauth-authorization-server/src/main/java/com/github/congyh/OauthAuthorizationServerApplication.java
+++ oauth-authorization-server/src/main/java/com/github/congyh/OauthAuthorizationServerApplication.java
@@ -2,7 +2,11 @@ package com.github.congyh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+@EnableAuthorizationServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OauthAuthorizationServerApplication {
 
