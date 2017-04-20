787de89665bd78f887def36c343289e4d96ff226
congyh <yihao.cong@outlook.com>
2017-04-20 15:34:28 +0800

    [WIP] Feigin Hystrix降级支持

--- feign/src/main/java/com/github/congyh/service/ConsumerServiceThroughEurekaServer.java
+++ feign/src/main/java/com/github/congyh/service/ConsumerServiceThroughEurekaServer.java
@@ -1,6 +1,5 @@
 package com.github.congyh.service;
 
-import com.github.congyh.service.impl.ConsumerServiceThroughEurekaServerImpl;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,11 +7,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * @author <a href='mailto:yihao.cong@outlook.com'>Cong Yihao</a>
  */
-@FeignClient(value = "EUREKA-CLIENT-SERVICE", fallback = ConsumerServiceThroughEurekaServerImpl.class) // 自带负载均衡, 底层也是通过ribbon实现的, 这里还添加了Hystrix熔断支持
-//@FeignClient(value = "EUREKA-CLIENT-SERVICE")
+// 自带负载均衡, 底层也是通过ribbon实现的, 这里还添加了Hystrix熔断支持
+@FeignClient(
+    value = "EUREKA-CLIENT-SERVICE",
+    fallback = ConsumerServiceThroughEurekaServer
+        .ConsumerServiceThroughEurekaServerFallBack.class)
 public interface ConsumerServiceThroughEurekaServer {
 
     // 这里注意接口定义要与被消费的REST服务一致
     @GetMapping(value = "/add")
     Integer add(@RequestParam("a") Integer a, @RequestParam("b") Integer b);
+
+    /**
+     * Hystrix的fallback支持
+     */
+    static class ConsumerServiceThroughEurekaServerFallBack implements ConsumerServiceThroughEurekaServer{
+
+        /**
+         * 本方法是用于支持熔断的功能降级
+         */
+        @Override
+        public Integer add(Integer a,Integer b) {
+            return -9999;
+        }
+    }
 }
+
+
